fe98d8e0a46607cfb315bef8b1c700168d136e6a
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-05-11 20:05:16 +0200

    cambio, no veas esto ruben

--- src/main/java/Ejercicio1/main.java
+++ src/main/java/Ejercicio1/main.java
@@ -1,6 +1,26 @@
 package Ejercicio1;
-public class main implements Prueba{
+public class main{
+    public static void prueba(){
+        ElementFactory factoryA = new FactoryElementoA();
+        ElementFactory factoryB = new FactoryElementoB();
+
+        Elemento elementoA1 = factoryA.crearElementoSimple();
+        Elemento elementoA2 = factoryA.crearElementoSimple();
+        Elemento elementoB1 = factoryB.crearElementoSimple();
+        Elemento elementoB2 = factoryB.crearElementoSimple();
+
+        Kit kit1 = new Kit();
+        kit1.añadirElemento(elementoA1);
+        kit1.añadirElemento(elementoA2);
+
+        Kit kit2 = new Kit();
+        kit2.añadirElemento(elementoB1);
+        kit2.añadirElemento(elementoB2);
+
+        System.out.println("Precio del kit1: " + kit1.getPrecio() + "€  --Código de productos: "+ elementoA1.getCodigo() + " "+ elementoA2.getCodigo());
+        System.out.println("Precio del kit2: " + kit2.getPrecio() + "€  --Código de productos: "+ elementoB1.getCodigo() + " "+ elementoB2.getCodigo());
+    }
     public static void main(String[] args) {
-        Prueba.prueba();
+        prueba();
     }
 }
